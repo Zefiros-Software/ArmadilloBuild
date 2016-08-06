baf3ad71f4a3f059ca34776847b0e6ac88d54108
PaulVisscher <paul.pev@hotmail.com>
2016-08-06 15:20:24 +0200

    New zpm style

---
+++ .build.1.lua
@@ -0,0 +1,29 @@
+
+project "Armadillo"
+
+    language "C++"
+    kind "Utility"
+
+    files {        
+        "include/**.hpp",
+        "include/armadillo" 
+    }
+
+    zpm.export [[
+        includedirs "include/"
+    ]]
+
+    if zpm.option( "Cpp11" ) then
+
+        zpm.export [[
+            flags "C++11"
+            defines {
+                "ARMA_SURPRESS_CXX11_WARNING",
+                "ARMA_USE_CXX11",
+            }
+        ]]
+    else
+        zpm.export [[
+            defines "ARMA_DONT_USE_CXX11"
+        ]]
+    end
