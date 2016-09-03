c6aed2c4e1695e471bce91151cc570ddc9d28e51
PaulVisscher <paul.pev@hotmail.com>
2016-09-03 17:01:50 +0200

    Add auto mkl linking

--- .build.1.lua
+++ .build.1.lua
@@ -25,3 +25,46 @@ project "Armadillo"
             defines "ARMA_DONT_USE_CXX11"
         ]]
     end
+
+    if zpm.setting( "Lapack" ) == "Auto" then
+
+        filter "architecture:x86_64"
+            local mkl = os.getenv("ICPP_COMPILER16") .. "/mkl/lib/intel64/"
+
+            zpm.export [[
+                includedirs {
+                    os.getenv("ICPP_COMPILER16") .. "/mkl/include"
+                }	
+
+                links {            
+                    mkl .. "mkl_blas95_lp64.lib",
+                    mkl .. "mkl_core.lib",
+                    mkl .. "mkl_intel_lp64.lib",
+                    mkl .. "mkl_lapack95_lp64.lib",
+                    mkl .. "mkl_rt.lib",
+                    mkl .. "mkl_sequential.lib",
+                    mkl .. "mkl_tbb_thread.lib"
+                }
+            ]]
+
+        filter "architecture:x86"
+            local mkl = os.getenv("ICPP_COMPILER16") .. "/mkl/lib/ia32/"
+
+            zpm.export [[
+                includedirs {
+                    os.getenv("ICPP_COMPILER16") .. "/mkl/include"
+                }	
+
+                links {            
+                    mkl .. "mkl_blas95.lib",
+                    mkl .. "mkl_core.lib",
+                    mkl .. "mkl_intel_c.lib",
+                    mkl .. "mkl_lapack95.lib",
+                    mkl .. "mkl_rt.lib",
+                    mkl .. "mkl_sequential.lib",
+                    mkl .. "mkl_tbb_thread.lib"
+                }
+            ]]
+
+        filter {}
+    end
